994c8ec56d824212f1a5e22b87340ee2dac13376
Mokhamad Arfan Wicaksono <marfanw@gmail.com>
2020-05-08 11:29:31 +0700

    finished v0.0

---
+++ count-bud-app.py
@@ -0,0 +1,83 @@
+# Code for counting image based on segmentation
+
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+import sys, getopt
+
+def main(argv):
+
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print('count-bud-app.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('count-bud-app.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+    print('Input file is "', inputfile)
+    print('Output file is "', outputfile)
+
+    original = cv.imread(inputfile)
+
+    # Convert image in grayscale
+    gray_im = cv.cvtColor(original, cv.COLOR_BGR2GRAY)
+    # plt.subplot(231)
+    # plt.title('Grayscale image')
+    # plt.imshow(gray_im, cmap="gray", vmin=0, vmax=255)
+
+    # Contrast adjusting with gamma correction y = 1.2
+    gray_correct = np.array(255 * (gray_im / 255) ** 1.2 , dtype='uint8')
+    # plt.subplot(232)
+    # plt.title('Gamma Correction y= 1.2')
+    # plt.imshow(gray_correct, cmap="gray", vmin=0, vmax=255)
+
+    # Contrast adjusting with histogramm equalization
+    gray_equ = cv.equalizeHist(gray_im)
+    # plt.subplot(233)
+    # plt.title('Histogram equilization')
+    # plt.imshow(gray_correct, cmap="gray", vmin=0, vmax=255)
+
+    # Local adaptative threshold
+    # thresh = cv.adaptiveThreshold(gray_correct, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 255, 0)
+    ret,thresh = cv.threshold(gray_correct, 50,255,cv.THRESH_BINARY)
+    # thresh = cv.bitwise_not(thresh)
+    # plt.subplot(234)
+    # plt.title('Local adapatative Threshold')
+    # plt.imshow(thresh, cmap="gray", vmin=0, vmax=255)
+
+    # Dilatation et erosion
+    kernel = np.ones((15,15), np.uint8)
+    img_dilation = cv.dilate(thresh, kernel, iterations=1)
+    img_erode = cv.erode(img_dilation,kernel, iterations=1)
+    # clean all noise after dilatation and erosion
+    img_erode = cv.medianBlur(img_erode, 7)
+    # plt.subplot(235)
+    # plt.title('Dilatation + erosion')
+    # plt.imshow(img_erode, cmap="gray", vmin=0, vmax=255)
+
+    # Labeling
+    ret, labels = cv.connectedComponents(img_erode)
+    label_hue = np.uint8(179 * labels / np.max(labels))
+    blank_ch = 255 * np.ones_like(label_hue)
+    labeled_img = cv.merge([label_hue, blank_ch, blank_ch])
+    labeled_img = cv.cvtColor(labeled_img, cv.COLOR_HSV2BGR)
+    labeled_img[label_hue == 0] = 0
+
+    # plt.subplot(236)
+    # plt.title('Objects counted:'+ str(ret-1))
+    # plt.imshow(labeled_img)
+    print('objects number is:', ret-1)
+
+    cv.imwrite(outputfile, labeled_img)
+    # plt.show()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
--- img-seg.py
+++ img-seg.py
@@ -3,32 +3,33 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
+import sys, getopt
 
-original = cv.imread()
+original = cv.imread('sample-image/cotton1.jpg')
 
 # Convert image in grayscale
 gray_im = cv.cvtColor(original, cv.COLOR_BGR2GRAY)
-plt.subplot(221)
+plt.subplot(231)
 plt.title('Grayscale image')
 plt.imshow(gray_im, cmap="gray", vmin=0, vmax=255)
 
 # Contrast adjusting with gamma correction y = 1.2
-
 gray_correct = np.array(255 * (gray_im / 255) ** 1.2 , dtype='uint8')
-plt.subplot(222)
+plt.subplot(232)
 plt.title('Gamma Correction y= 1.2')
 plt.imshow(gray_correct, cmap="gray", vmin=0, vmax=255)
+
 # Contrast adjusting with histogramm equalization
 gray_equ = cv.equalizeHist(gray_im)
-plt.subplot(223)
+plt.subplot(233)
 plt.title('Histogram equilization')
 plt.imshow(gray_correct, cmap="gray", vmin=0, vmax=255)
 
 # Local adaptative threshold
-
-thresh = cv.adaptiveThreshold(gray_correct, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 255, 19)
-thresh = cv.bitwise_not(thresh)
-plt.subplot(221)
+# thresh = cv.adaptiveThreshold(gray_correct, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 255, 0)
+ret,thresh = cv.threshold(gray_correct, 50,255,cv.THRESH_BINARY)
+# thresh = cv.bitwise_not(thresh)
+plt.subplot(234)
 plt.title('Local adapatative Threshold')
 plt.imshow(thresh, cmap="gray", vmin=0, vmax=255)
 
@@ -38,7 +39,7 @@ img_dilation = cv.dilate(thresh, kernel, iterations=1)
 img_erode = cv.erode(img_dilation,kernel, iterations=1)
 # clean all noise after dilatation and erosion
 img_erode = cv.medianBlur(img_erode, 7)
-plt.subplot(221)
+plt.subplot(235)
 plt.title('Dilatation + erosion')
 plt.imshow(img_erode, cmap="gray", vmin=0, vmax=255)
 
@@ -50,8 +51,9 @@ labeled_img = cv.merge([label_hue, blank_ch, blank_ch])
 labeled_img = cv.cvtColor(labeled_img, cv.COLOR_HSV2BGR)
 labeled_img[label_hue == 0] = 0
 
-plt.subplot(222)
+plt.subplot(236)
 plt.title('Objects counted:'+ str(ret-1))
 plt.imshow(labeled_img)
 print('objects number is:', ret-1)
+
 plt.show()
