29992af3a756344f26162bdb9be4f9b9a76d40f0
Mokhamad Arfan Wicaksono <marfanw@gmail.com>
2020-05-08 10:56:07 +0700

    finished code

--- img-seg.py
+++ img-seg.py
@@ -1,6 +1,10 @@
 # Code for counting image based on segmentation
 
 import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+original = cv.imread()
 
 # Convert image in grayscale
 gray_im = cv.cvtColor(original, cv.COLOR_BGR2GRAY)
@@ -20,3 +24,34 @@ plt.subplot(223)
 plt.title('Histogram equilization')
 plt.imshow(gray_correct, cmap="gray", vmin=0, vmax=255)
 
+# Local adaptative threshold
+
+thresh = cv.adaptiveThreshold(gray_correct, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 255, 19)
+thresh = cv.bitwise_not(thresh)
+plt.subplot(221)
+plt.title('Local adapatative Threshold')
+plt.imshow(thresh, cmap="gray", vmin=0, vmax=255)
+
+# Dilatation et erosion
+kernel = np.ones((15,15), np.uint8)
+img_dilation = cv.dilate(thresh, kernel, iterations=1)
+img_erode = cv.erode(img_dilation,kernel, iterations=1)
+# clean all noise after dilatation and erosion
+img_erode = cv.medianBlur(img_erode, 7)
+plt.subplot(221)
+plt.title('Dilatation + erosion')
+plt.imshow(img_erode, cmap="gray", vmin=0, vmax=255)
+
+# Labeling
+ret, labels = cv.connectedComponents(img_erode)
+label_hue = np.uint8(179 * labels / np.max(labels))
+blank_ch = 255 * np.ones_like(label_hue)
+labeled_img = cv.merge([label_hue, blank_ch, blank_ch])
+labeled_img = cv.cvtColor(labeled_img, cv.COLOR_HSV2BGR)
+labeled_img[label_hue == 0] = 0
+
+plt.subplot(222)
+plt.title('Objects counted:'+ str(ret-1))
+plt.imshow(labeled_img)
+print('objects number is:', ret-1)
+plt.show()
